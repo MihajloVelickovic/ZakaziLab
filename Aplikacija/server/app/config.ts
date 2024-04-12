a4c4f4c9007b55b82ab6825b4730e5bd919b92cc
Mihajlo Velickovic <permutator@elfak.rs>
2024-04-12 23:43:17 +0200

    Ispravljeno formatiranje da kod staje u <80 kolona

--- Aplikacija/server/app/config.ts
+++ Aplikacija/server/app/config.ts
@@ -1,14 +1,17 @@
 import dotenv from "dotenv";
-import path from 'path';
+import path from "path";
 
-const envFilePath = process.platform.startsWith('win') ?
-    path.join(__dirname, 'config.env') : path.join(__dirname, '/.env');
-
+const envFilePath = process.platform.startsWith("win") ?
+                    path.join(__dirname, "config.env") : 
+                    path.join(__dirname, "/.env");
 
 dotenv.config({path: envFilePath});
 
-const URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@zakazi-lab-cluster.bt5bepa.mongodb.net/?retryWrites=true&w=majority&appName=Zakazi-Lab-Cluster`
-
-//console.log(URI);
+const URI = `mongodb+srv://${process.env.MONGO_USER}:`+
+                           `${process.env.MONGO_PASSWORD}@`+
+                           "zakazi-lab-cluster.bt5bepa.mongodb.net/?"+
+                           "retryWrites=true&"+
+                           "w=majority&"+
+                           "appName=Zakazi-Lab-Cluster"
 
 export {URI};
--- Aplikacija/server/app/index.ts
+++ Aplikacija/server/app/index.ts
@@ -7,11 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 mongoose.connect(URI)
         .then(() => {
             app.listen(process.env.SERVER_PORT, () => {
-                console.log(`Server running on port ${process.env.SERVER_PORT}`);
+                console.log(`Running on port ${process.env.SERVER_PORT}`);
             })
         })
         .catch((err) => {
