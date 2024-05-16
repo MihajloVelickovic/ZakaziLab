4e360b6e9ed12914bb69d49cd8c29c9c6cdf3290
Lazar Becic <lazarrr@elfak.rs>
2024-05-17 00:24:01 +0200

    Application - Client side - Added a basic template to work on.

---
+++ Aplikacija/client/src/index.js
@@ -0,0 +1,24 @@
+// //import React from 'react';
+// //import ReactDOM from 'react-dom/client';
+
+// const conatiner = document.getElementById("root");
+// const root = ReactDOM.createRoot(conatiner);
+
+// const myElement = <h1>{x < 10 ? "Hello" : "Goodbye"}</h1>;
+
+// class Car extends React.Component {
+//   render(){
+//     return <h2>
+//       Something is here, so it's not empty.
+//     </h2>
+//   }
+// }
+
+// function SmallCar() {
+//   return <h3>A little bittle small caw</h3>
+// }
+
+// var c = new Car();
+
+// root.render(<Car/>);
+
