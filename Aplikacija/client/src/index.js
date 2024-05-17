c2576b2405cca02db2809f758064aefa6fd93f3b
Lazar Becic <lazarrr@elfak.rs>
2024-05-17 20:03:18 +0200

    Aplikacija/Client izmenjeni fajlovi vezani za koriscenje React i ReactDOM-a

--- Aplikacija/client/src/index.js
+++ Aplikacija/client/src/index.js
@@ -1,24 +1,17 @@
-// //import React from 'react';
-// //import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// const conatiner = document.getElementById("root");
-// const root = ReactDOM.createRoot(conatiner);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// const myElement = <h1>{x < 10 ? "Hello" : "Goodbye"}</h1>;
-
-// class Car extends React.Component {
-//   render(){
-//     return <h2>
-//       Something is here, so it's not empty.
-//     </h2>
-//   }
-// }
-
-// function SmallCar() {
-//   return <h3>A little bittle small caw</h3>
-// }
-
-// var c = new Car();
-
-// root.render(<Car/>);
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
