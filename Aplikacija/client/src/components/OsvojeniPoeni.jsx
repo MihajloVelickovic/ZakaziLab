1f0f9605c1956564bf5bbf29eb4c7408344af76c
Lazar Becic <lazarrr@elfak.rs>
2024-05-30 19:46:56 +0200

    Dodat kod koji omogucava ispravno koriscenje fetcha od strane klijentske strane

--- Aplikacija/client/src/components/OsvojeniPoeni.jsx
+++ Aplikacija/client/src/components/OsvojeniPoeni.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 
+
+
 const OsvojeniPoeni = () => {
+
+    // testiranje fetcha za studente
+    const response = fetch("http://127.0.0.1:1738/student/FindAll")
+                    .then(res => res.json())
+                    .then(res=>
+                        console.log(res)
+                    );    
+    
     return (
-        <h2>Nula bajo moj, nula.</h2>
+        <>
+            <h2>Nula bajo moj, nula.</h2>
+        </>
+        
+        
     )
 }
 
