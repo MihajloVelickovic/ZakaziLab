35876da7c9a0bea905ad1b11761c33a1061d206e
Lazar Becic <lazarrr@elfak.rs>
2024-06-01 01:58:35 +0200

    Client: Added functions to show data fetched from the server side, mainly in Osvojeni Poeni component

--- Aplikacija/client/src/components/OsvojeniPoeni.jsx
+++ Aplikacija/client/src/components/OsvojeniPoeni.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/OsvojeniPoeni.css";
 
+const fetchData = async () => {
+  try {
+    const response = await fetch('http://127.0.0.1:1738/studentEntry/findAll');
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
+
+const ListItem = ({ _id, student, attendance, timeSlot, points, labName, __v }) => (
+  <li>
+    {_id} &mdash; {labName} &mdash; {student.index}
+  </li>
+);
+
+const ListItems = ({ items }) => (
+  <ul>
+    {items.map((props) => (
+      <ListItem key={props._id} {...props} />
+    ))}
+  </ul>
+);
 
 const OsvojeniPoeni = () => {
+  const [result, setResult] = useState([]);
 
-    // testiranje fetcha za studente
-    let Sindex = 18588;
-    let result;
-    // const response = fetch("http://127.0.0.1:1738/studentEntry/filteredFind")
-    //                 .then(res => res.json())
-    //                 .then(res=>
-    //                     res.forEach(p => console.log(p.labName))
-    //                 ).then(res => {
-    //                     result = res;
-    //                 });
+  useEffect(() => {
+    fetchData().then((res) => {
+        console.log('Fetched data:', res);
+      setResult(res);
+    }).catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+  }, []);
 
-    
-    const renderFunction = (result) => {
-        let element = document.querySelector('rezultati');
-        
-    }
-    
+  return (
+    <div>
+      {result.length > 0 && <ListItems items={result} />}
+    </div>
+  );
+}
+
+export default OsvojeniPoeni;
+
+
     // const yourDataObject = { labName: "OOP"};
     // , {
     //     method: 'POST',
@@ -29,31 +59,28 @@ const OsvojeniPoeni = () => {
     //     },
     //     body: JSON.stringify(yourDataObject)
 
-    const response = fetch("http://127.0.0.1:1738/studentEntry/findAll")
-                    .then(res => res.json())
-                    .then(data => {
-                        const filteredData = data.filter(entry => 
-                            entry.student.index === Sindex
-                        );
-                        //filteredData.forEach(p => console.log(p));
-                        return filteredData;
-                    })
-                    .then(result => {
-                        console.log(result); // If you want to do something with the result
-                    })
-                    .catch(error => console.error('Error:', error));
 
-
-    
-    return (
-        <>
-            <h2>Nula bajo moj, nula.</h2>
-            <div className="resultati">
-            </div>
-        </>
         
+    // return (
         
-    )
-}
+    //     <>
+    //         <h2>Nula bajo moj, nula.</h2>
+    //         <div className="resultati">
+    //         </div>
+    //     </>   
+    // )
 
-export default OsvojeniPoeni;
+
+       // const response = fetch("http://127.0.0.1:1738/studentEntry/findAll")
+    //                 .then(res => res.json())
+    //                 .then(data => {
+    //                     const filteredData = data.filter(entry => 
+    //                         entry.student.index === Sindex
+    //                     );
+    //                     //filteredData.forEach(p => console.log(p));
+    //                     return filteredData;
+    //                 })
+    //                 .then(result => {
+    //                     console.log(result); // If you want to do something with the result
+    //                 })
+    //                 .catch(error => console.error('Error:', error));
