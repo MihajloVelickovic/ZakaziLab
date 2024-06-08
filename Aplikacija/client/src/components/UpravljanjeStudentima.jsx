989e6eb4601cb9be429c74229ab1968746434d52
Jana Ratkov <janaratkov@elfak.rs>
2024-06-09 00:58:41 +0200

    Izmene kod azuriranja studenta

--- Aplikacija/client/src/components/UpravljanjeStudentima.jsx
+++ Aplikacija/client/src/components/UpravljanjeStudentima.jsx
@@ -1,218 +1,325 @@
 import React, { useEffect, useState } from "react";
-import axiosInstance from '../utils/axiosInstance';
-import "../styles/UpravljanjeStudentima.css"; 
+import axiosInstance from "../utils/axiosInstance";
+import "../styles/UpravljanjeStudentima.css";
 
 const fetchData = async () => {
-    try {
-        const response = await axiosInstance.get('/student/findAll');
-        const data = response.data;
-        console.log("fetched: ", data);
-        return data;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.get("/student/findAll");
+    const data = response.data;
+    console.log("fetched: ", data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 };
 
-const validateForm = (formData) => {
+const validateForm = (formData, isAdding) => {
     const errors = {};
     const placeholders = {
-        name: 'Name',
-        lastName: 'Last Name',
-        email: 'Email',
-        password: 'Password',
-        birthDate: 'Birth Date',
-        index: 'Index',
-        module: 'Module'
+      name: "Ime",
+      lastName: "Prezime",
+      email: "Email",
+      birthDate: "Datum rođenja",
+      privileges: "Privilegije",
+      password: "Šifra",
+      index: "Indeks",
+      module: "Modul",
     };
+  
     for (let key in formData) {
-        if (!formData[key]) {
-            errors[key] = `${placeholders[key]} is required!`;
-        }
+      if (!formData[key] && (isAdding || key !== "password")) {
+        errors[key] = `${placeholders[key]} is required!`;
+      }
     }
     return errors;
-};
+  };
+  
 
 const UpravljanjeStudentima = () => {
-    const [students, setStudents] = useState([]);
-    const [formData, setFormData] = useState({
-        name: '',
-        lastName: '',
-        email: '',
-        password: '',
-        birthDate: '',
-        index: '',
-        module: ''
+  const [students, setStudents] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    birthDate: "",
+    privileges: "",
+    password: "",
+    index: "",
+    module: "",
+  });
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentStudentId, setCurrentStudentId] = useState(null);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [existingPassword, setExistingPassword] = useState("");
+
+  useEffect(() => {
+    fetchData()
+      .then((json) => {
+        setStudents(json);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const clearForm = () => {
+    setFormData({
+      name: "",
+      lastName: "",
+      email: "",
+      birthDate: "",
+      privileges: "",
+      password: "",
+      index: "",
+      module: "",
     });
-    const [isEditing, setIsEditing] = useState(false);
-    const [currentStudentId, setCurrentStudentId] = useState(null);
-    const [showPassword, setShowPassword] = useState(false);
-    const [successMessage, setSuccessMessage] = useState('');
+    setIsEditing(false);
+    setCurrentStudentId(null);
+  };
 
-    useEffect(() => {
-        fetchData()
-        .then(json => {
-            setStudents(json);
-        })
-        .catch((error) => {
-            console.error('Error fetching data:', error);
-        });
-    }, []);
+  const addStudent = async () => {
+    try {
+      formData.privileges = "student";
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+      const errorList = validateForm(formData,true);
+      if (Object.keys(errorList).length !== 0) {
+        const errorMessage = Object.values(errorList).join("\n");
+        alert(errorMessage);
+        return;
+      }
 
-    const clearForm = () => {
-        setFormData({
-            name: '',
-            lastName: '',
-            email: '',
-            password: '',
-            birthDate: '',
-            index: '',
-            module: ''
-        });
-        setIsEditing(false);
-        setCurrentStudentId(null);
-    };
+      const response = await axiosInstance.post("/student/add", formData);
+      const newStudent = response.data;
+      setStudents([...students, newStudent]);
+      setSuccessMessage("Student dodat uspešno.");
+      clearForm();
+    } catch (error) {
+      setSuccessMessage("Student nije dodat u bazu");
+      console.error("Error adding student:", error);
+    }
+  };
 
-    const addStudent = async () => {
-        try {
-            formData.privileges = 'student';
-            
-            const errorList = validateForm(formData);
-            if (Object.keys(errorList).length !== 0) {
-                const errorMessage = Object.values(errorList).join('\n');
-                alert(errorMessage);
-                return;
-            }
+  const updateStudent = async () => {
+    try {
+      if (!currentStudentId) {
+        console.error("No student ID found for updating.");
+        return;
+      }
 
-            const response = await axiosInstance.post('/student/add', formData);
-            const newStudent = response.data;
-            setStudents([...students, newStudent]);
-            setSuccessMessage('Student dodat uspešno.');
-            clearForm();
-        } catch (error) {
-            console.error('Error adding student:', error);
-        }
-    };
+      const errorList = validateForm(formData,false);
+      if (Object.keys(errorList).length !== 0) {
+        const errorMessage = Object.values(errorList).join("\n");
+        alert(errorMessage);
+        return;
+      }
 
-    const updateStudent = async () => {
-        try {
-            if (!currentStudentId) {
-                console.error('No student ID found for updating.');
-                return;
-            }
+      const passwordToUpdate = existingPassword;
+      console.log("Sifrica je" + passwordToUpdate)
+      const updateData = { ...formData, password: passwordToUpdate };
 
-            const errorList = validateForm(formData);
-            if (Object.keys(errorList).length !== 0) {
-                const errorMessage = Object.values(errorList).join('\n');
-                alert(errorMessage);
-                return;
-            }
-            
-            const response = await axiosInstance.patch(`/student/update/${currentStudentId}`, formData);
-            const updatedStudent = response.data;
+      const response = await axiosInstance.patch(
+        `/student/update/${currentStudentId}`,
+        updateData
+      );
+      const updatedStudent = response.data;
 
-            setStudents(students.map(student => (student._id === currentStudentId ? updatedStudent : student)));
-            setSuccessMessage('Student promenjen uspešno.');
-            clearForm();
-        } catch (error) {
-            console.error('Error updating student:', error);
-        }
-    };
+      setStudents(
+        students.map((student) =>
+          student._id === currentStudentId ? updatedStudent : student
+        )
+      );
+      setSuccessMessage("Student promenjen uspešno.");
+      clearForm();
+    } catch (error) {
+      console.error("Error updating student:", error);
+      setSuccessMessage("Student nije promenjen");
+    }
+  };
 
-    const editStudent = (student) => {
-        const formattedBirthDate = student.birthDate ? new Date(student.birthDate).toISOString().split('T')[0] : '';
-        setFormData({
-            name: student.name,
-            lastName: student.lastName,
-            email: student.email || '',
-            password: student.password || '', 
-            birthDate: formattedBirthDate,
-            index: student.index,
-            module: student.module || ''
-        });
-        setIsEditing(true);
-        setCurrentStudentId(student._id);
-    };
+  const editStudent = (student) => {
+    const formattedBirthDate = student.birthDate
+      ? new Date(student.birthDate).toISOString().split("T")[0]
+      : "";
+    setFormData({
+      name: student.name,
+      lastName: student.lastName,
+      email: student.email || "",
+      privileges: student.privileges,
+      password: "", 
+      birthDate: formattedBirthDate,
+      index: student.index,
+      module: student.module || "",
+    });
 
-    const deleteStudent = async (studentId) => {
-        try {
-            const response = await axiosInstance.delete(`/student/delete/${studentId}`);
-            console.log(response.data.message);
-            setStudents(students.filter(student => student._id !== studentId));
-            setSuccessMessage('Student obrisan uspešno.');
-            clearForm();
-        } catch (error) {
-            console.error('Error deleting student:', error);
-        }
-    };
+    setExistingPassword(student.password);
+    setIsEditing(true);
+    setCurrentStudentId(student._id);
+  };
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setSuccessMessage('');
-        }, 3000); // Adjust the timeout duration as needed
-        return () => clearTimeout(timer);
-    }, [successMessage]);
+  const deleteStudent = async (studentId) => {
+    try {
+      const response = await axiosInstance.delete(
+        `/student/delete/${studentId}`
+      );
+      console.log(response.data.message);
+      setStudents(students.filter((student) => student._id !== studentId));
+      setSuccessMessage("Student obrisan uspešno.");
+      clearForm();
+    } catch (error) {
+      console.error("Error deleting student:", error);
+      setSuccessMessage("Student nije obrisan");
+    }
+  };
 
-    return (
-        <div className="wrapper">
-            {successMessage && <div className="success-message">{successMessage}</div>}
-            <div className="input-group">
-                <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name" />
-                <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} placeholder="Last Name" />
-            </div>
-            <div className="input-group">
-                <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" />
-                <input type={showPassword ? "text" : "password"} name="password" value={formData.password} onChange={handleChange} placeholder="Password" />
-            </div>
-            <div className="input-group big">
-                <input type="date" name="birthDate" value={formData.birthDate} onChange={handleChange} placeholder="Birth Date" />
-            </div>
-            <div className="input-group">
-                <input type="number" name="index" value={formData.index} onChange={handleChange} placeholder="Index" />
-                <input type="text" name="module" value={formData.module} onChange={handleChange} placeholder="Module" />
-            </div>
-            <div className="input-group">
-                <button className="btn-submit" onClick={isEditing ? updateStudent : addStudent}>
-                    {isEditing ? 'Izmeni studenta' : 'Dodaj studenta'}
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSuccessMessage("");
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [successMessage]);
+
+  const renderPasswordInput = () => {
+    if (isEditing) {
+      return (
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          placeholder="Šifra"
+          disabled
+        />
+      );
+    } else {
+      return (
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="Šifra"
+        />
+      );
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      {successMessage && (
+        <div className="success-message">{successMessage}</div>
+      )}
+      <div className="input-group">
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          placeholder="Ime"
+        />
+        <input
+          type="text"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+          placeholder="Prezime"
+        />
+      </div>
+      <div className="input-group">
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Email"
+        />
+        <input
+          type="privileges"
+          name="privileges"
+          value={formData.privileges}
+          onChange={handleChange}
+          placeholder="Privilegije"
+        />
+      </div>
+      <div className="input-group big">
+        <input
+          type="date"
+          name="birthDate"
+          value={formData.birthDate}
+          onChange={handleChange}
+          placeholder="Datum rođenja"
+        />
+        {renderPasswordInput()}
+      </div>
+      <div className="input-group">
+        <input
+          type="number"
+          name="index"
+          value={formData.index}
+          onChange={handleChange}
+          placeholder="Indeks"
+        />
+        <input
+          type="text"
+          name="module"
+          value={formData.module}
+          onChange={handleChange}
+          placeholder="Modul"
+        />
+      </div>
+      <div className="input-group">
+        <button
+          className="btn-submit"
+          onClick={isEditing ? updateStudent : addStudent}
+        >
+          {isEditing ? "Izmeni studenta" : "Dodaj studenta"}
+        </button>
+        <button className="btn-clear" onClick={clearForm}>
+          Poništi
+        </button>
+      </div>
+      <table className="table-students">
+        <thead>
+          <tr>
+            <th scope="col">Indeks</th>
+            <th scope="col">Ime</th>
+            <th scope="col">Prezime</th>
+            <th scope="col">Izmeni</th>
+            <th scope="col">Obriši</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student, index) => (
+            <tr key={index}>
+              <td>{student.index}</td>
+              <td>{student.name}</td>
+              <td>{student.lastName}</td>
+              <td className="action-column">
+                <button
+                  className="btn-submit small"
+                  onClick={() => editStudent(student)}
+                >
+                  Izmeni
                 </button>
-                <button className="btn-clear" onClick={clearForm}>
-                    Poništi
+              </td>
+              <td className="action-column">
+                <button
+                  className="btn-submit small"
+                  onClick={() => deleteStudent(student._id)}
+                >
+                  Obriši
                 </button>
-            </div>
-            <table className="table-students">
-                <thead>
-                    <tr>
-                        <th scope="col">Indeks</th>
-                        <th scope="col">Ime</th>
-                        <th scope="col">Prezime</th>
-                        <th scope="col">Izmeni</th> 
-                        <th scope="col">Obriši</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {students.map((student, index) => (
-                        <tr key={index}>
-                            <td>{student.index}</td>
-                            <td>{student.name}</td>
-                            <td>{student.lastName}</td>
-                            <td className="action-column">
-                                <button className="btn-submit small" onClick={() => editStudent(student)}>Izmeni</button>
-                            </td>
-                            <td className="action-column">
-                                <button className="btn-submit small" onClick={() => deleteStudent(student._id)}>Obriši</button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
-}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default UpravljanjeStudentima;
-
