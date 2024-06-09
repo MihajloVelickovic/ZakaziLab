c1dddb2cd6f8880056be4ba24e653225eebb368b
Jana Ratkov <janaratkov@elfak.rs>
2024-06-09 20:42:46 +0200

    Napravljena pocetna stranica za studenta sa osnovnim informacijama

--- Aplikacija/client/src/components/PageHome.jsx
+++ Aplikacija/client/src/components/PageHome.jsx
@@ -1,30 +1,47 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axiosInstance from '../utils/axiosInstance'
+
+var user = null;
+if (localStorage.getItem('user')){
+    user = JSON.parse(localStorage.getItem('user'));
+    console.log("user sa localStorage-a: ", user);
+}
 
 const PageHome = ({role}) => {
+    const [entries, setEntries] = useState([]);
+    const [studentIndex, setStudentIndex] = useState(null);
+
+
     const renderStudentHomePage = () => (
         <>
-            <h2>Student: This is what will he shown first, maybe we can put like: This is the page for student... you can do this...</h2>
+            <div className="user-info">
+                <h2>Prijavljen korisnik: </h2>
+                <h2>Ime: {user.name}</h2>
+                <h2>Prezime: {user.lastName}</h2>
+                <h2>Email: {user.email}</h2>
+            </div>
         </>
     );
-    const renderProfessorHomePage = () => (
-        <>
-            <h2>Professor: This is what will he shown first, maybe we can put like: This is the page for student... you can do this...</h2>
-        </>
-    );
-    const renderAdminHomePage = () => (
-        <>
-            <h2>Admin: This is what will he shown first, maybe we can put like: This is the page for student... you can do this...</h2>
-        </>
-    );
+    // const renderProfessorHomePage = () => (
+    //     <>
+    //         <h2>Professor: This is what will he shown first, maybe we can put like: This is the page for student... you can do this...</h2>
+    //     </>
+    // );
+    // const renderAdminHomePage = () => (
+    //     <>
+    //         <h2>Admin: This is what will he shown first, maybe we can put like: This is the page for student... you can do this...</h2>
+    //     </>
+    // );
 
     let renderContext;
     switch (role) {
-        case 'admin':
-            renderContext = renderAdminHomePage();
-            break;
-        case 'professor':
-            renderContext = renderProfessorHomePage();
-            break;
+        // case 'admin':
+        //     renderContext = renderAdminHomePage();
+        //     break;
+        // case 'professor':
+        //     renderContext = renderProfessorHomePage();
+        //     break;
         case 'student':
         default:
             renderContext = renderStudentHomePage();
@@ -34,7 +51,6 @@ const PageHome = ({role}) => {
     return (
         <>
         <div>
-            <h2>This is the home section</h2>
             {renderContext}
         </div>
         </>
--- Aplikacija/client/src/components/Student.jsx
+++ Aplikacija/client/src/components/Student.jsx
@@ -69,30 +69,3 @@ const Student = () => {
 }
 
 export default Student;
-
-//position: 'fixed', top: '10px', left: '10px', 
-//id="collapseClass" className="collapse"
-
-
-
-/*
-<aside>
-                <h2>Working on this</h2>
-                <Router>
-                    <NavigationBar />
-                    <Switch>
-                        <Route exact path='/' component={StudentHome}/>
-                        <Route path='/lab' component={LaboratorijskaVezba}/>
-                        <Route path='/poeni' component={OsvojeniPoeni}/>
-                        <Route path='/aktivnosti' component={KalendarAktivnosti}/>
-                    </Switch>
-                </Router>
-            </aside>
-            <main>
-                <h2>Something in main</h2>
-                <br></br>
-                <h3>Work in progress bro</h3>
-                <Button>Useless bootstrap button</Button>
-                <p>I should maybe reduce the width of main</p>
-            </main>
-*/
